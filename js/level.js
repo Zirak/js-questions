17a3b1f2b23482536d3d61dcddd14be0ed67a3a6
Zirak <benstav@gmail.com>
2013-10-08 16:15:13 +0000

    restructured the js logic, added the timer

--- js/level.js
+++ js/level.js
@@ -1,8 +1,64 @@
 //hahaha. this is so horrible. state everywhere.
-var level;
-var levels = [
+var game = {
+    level  : null,
+    editor : null,
+    worker : null,
+
+    reset : function () {
+        this.editor = ace.edit('editor');
+        this.editor.setTheme('ace/theme/twilight');
+        this.editor.getSession().setMode('ace/mode/javascript');
+
+        timer.stop();
+    },
+
+    runCode : function (evt) {
+        console.log('running code');
+        timer.pause();
+
+        if (evt && evt.preventDefault) {
+            evt.preventDefault();
+        }
+
+        if (!this.worker) {
+            this.setupWorker();
+        }
+
+        this.worker.postMessage({
+            code : this.editor.getValue(),
+            name : this.level.name,
+            tests: this.level.tests
+        });
+    },
+
+    nextLevel : function () {
+        console.log('progressing level');
+        this.level = this.levels.shift();
+        this.calibrateEditor();
+        timer.play();
+    },
+
+    setupWorker : function () {
+        this.worker = new Worker('js/evalWorker.js');
+
+        this.worker.onmessage = function (evt) {
+            console.log('worker.onmessage');
+            logger.logWorkerEvent(evt);
+        };
+    },
+
+    calibrateEditor : function () {
+        this.editor.setReadOnly(false);
+        this.editor.setValue(this.level.code);
+        this.editor.focus();
+        this.editor.clearSelection();
+        this.editor.moveCursorToPosition(this.level.cursor);
+    }
+};
+
+game.levels = [
     {
-        title : 'I like moose',
+        text : 'Hit the button to the left, or Ctrl+Enter/Mac+Enter to submit',
         name : 'square',
 
         code : ['box.square = function (x) {',
@@ -27,68 +83,104 @@ var levels = [
     }
 ];
 
+var timer = {
+    elem : document.getElementById('timer'),
 
-var editor = ace.edit('editor');
-editor.setTheme('ace/theme/twilight');
-editor.getSession().setMode('ace/mode/javascript');
+    interval : 100,
+    time : 0,
 
-var worker = setupWorker();
-function setupWorker () {
-    var ret = new Worker('js/evalWorker.js');
-    ret.onmessage = function (evt) {
-        logger.logWorkerEvent(evt);
-    };
-    return ret;
-}
+    play : function () {
+        console.log('play');
+        this.intervalId = setInterval(this.tick.bind(this), this.interval);
+        console.log('interval id:', this.intervalId);
+    },
+    pause : function () {
+        console.log('pause', this.intervalId);
+        clearInterval(this.intervalId);
+    },
+    reset : function () {
+        console.log('reset');
+        this.time = 0;
+    },
+    stop : function () {
+        console.log('stop');
+        this.pause();
+        this.reset();
+    },
+
+    tick : function () {
+        this.time += this.interval;
+        this.write();
+    },
+
+    write : function (elem) {
+        var ms = this.time,
+            secs, mins;
+
+        secs = Math.floor(ms / 1000);
+        ms %= 1000;
+        mins = Math.floor(secs / 60);
+        secs %= 60;
+
+        var format = [mins, secs].map(function (t) {
+            return (t < 10 ? '0' : '') + t;
+        }).join(':');
+
+        this.elem.textContent = format;
+    }
+};
 
 var logger = {
     elem : document.getElementById('log-wrapper'),
 
+    log : function (text) {
+        this.addLine(this.createLine(text));
+    },
+
     logWorkerEvent : function (evt) {
-        var line = document.createElement('div'),
-        data = evt.data;
+        var data = evt.data,
+            line = this.createLine(data.message);
 
-        line.classList.add('log-line');
         line.classList.add('log-' + data.type);
+        this.addLine(line);
+    },
 
-        line.textContent = data.message;
-
+    addLine : function (line) {
         this.elem.appendChild(line);
         this.elem.scrollTop = this.elem.scrollHeight
+    },
+
+    createLine : function (text) {
+        var line = document.createElement('div');
+        line.classList.add('log-line');
+
+        if (text) {
+            line.textContent = text;
+        }
+
+        return line;
+    },
+
+    clear : function () {
+        var child;
+        while (child = this.elem.firstElementChild) {
+            this.elem.removeChild(child);
+        }
     }
 };
 
-function nextLevel () {
-    level = levels.shift();
-    calibrateEditorTo(level);
-}
+document.getElementById('run-button').onclick = function (evt) {
+    game.runCode(evt);
+};
 
-function calibrateEditorTo (level) {
-    editor.setReadOnly(false);
-    editor.setValue(level.code);
-    editor.focus();
-    editor.clearSelection();
-    editor.moveCursorToPosition(level.cursor);
-}
-
-nextLevel();
-
-function runCode (evt) {
-    if (evt && evt.preventDefault) {
-        evt.preventDefault();
-    }
-
-    worker.postMessage({
-        code : editor.getValue(),
-        name : level.name,
-        tests: level.tests
-    });
-}
-
-document.getElementById('run-button').onclick = runCode;
 document.onkeydown = function (evt) {
-    //13 is the keycode for Enter
-    if (evt.which === 13 && (evt.ctrlKey || evt.metaKey)) {
-        runCode(evt);
+    var enterKeyCode = 13;
+    if (evt.which === enterKeyCode && (evt.ctrlKey || evt.metaKey)) {
+        game.runCode(evt);
     }
 };
+
+game.reset();
+
+game.nextLevel();
+timer.reset();
